8f009273541b06cff34c1aea709fac22d07bc5f8
sapo83 <SarahASA@gmail.com>
2020-05-20 21:32:34 -0400

    initial commit

---
+++ driver.R
@@ -0,0 +1,10 @@
+library(datadrivencv)
+
+# Sets up files in current working directory
+datadrivencv::use_datadriven_cv(
+  full_name = "Nick Strayer",
+  data_location = "/Users/sarah/Documents/Github_CV",
+  pdf_location = "https://github.com/sapo83/Github_CV/raw/master/entwistle_cv.pdf",
+  html_location = "https://github.com/sapo83/index.html",
+  source_location = "https://github.com/sapo83/Github_CV"
+)
